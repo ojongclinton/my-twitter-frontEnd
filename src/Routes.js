cb27d3c23f3d602e6b0438bf903d97b0ad5494cd
ojong <82442816+ojongclinton@users.noreply.github.com>
2023-07-18 23:58:28 +0100

    layout

--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,43 @@
 import React from 'react'
-import { Route,Routes } from 'react-router-dom'
+import { Outlet, Route,Routes } from 'react-router-dom'
 import { CheckUserIsNotAuth,CheckUserisAuth } from './Components/Other/CheckAuth'
 import AuthComponent from './Components/Authentication/AuthComponent';
 import {AuthHeader,NoAuthHeader} from './Components/Layout/Header'
 import {AuthFooter,NoAuthFooter} from './Components/Layout/Footer'
+import { Grid } from '@mui/material';
+import Right from './Components/Layout/Right';
+import Left from './Components/Layout/Left';
 
 {/*User must be authenticated before he can have access to any of these routes*/}
+{/*---/----*/}
+
+const LayOut = () =>{
+    return(
+        <div>
+            <Grid container>
+                <Grid item xl={2} lg={2}>
+                    <Right />
+                </Grid>
+                <Grid item xl={7} lg={7}>
+                    <Outlet />
+                </Grid>
+                <Grid item xl={3} lg={3}>
+                    <Left />
+                </Grid>
+            </Grid> 
+        </div>
+    )
+}
 export const TwitController =()=>{
     return(
         <>
             <AuthHeader />
                 <Routes>
                     <Route element={<CheckUserisAuth />}>
-                        <Route path="/" element={<p>Home page of the / path</p>}/>
-                        <Route path="/lol" element={<p>lol page of the / path</p>}/>
+                        <Route element={<LayOut />}>
+                            <Route path="/" element={<p>Home page of the / path</p>}/>
+                            <Route path="/lol" element={<p>lol page of the / path</p>}/>
+                        </Route>
                     </Route>
                 </Routes>
             <AuthFooter />
@@ -22,6 +46,7 @@ export const TwitController =()=>{
 }
 
 {/*Authenticated users can't access these routes*/}
+{/*---/auth----*/}
 export const Auth=()=>{
     return(
         <>
@@ -36,12 +61,19 @@ export const Auth=()=>{
 }
 
 {/*Authenticated users can't access these routes*/}
+{/*---/explore----*/}
 export const ExploreController =()=>{
     return(
-    <Routes>
-         <Route element={<CheckUserIsNotAuth />}>
-            <Route path="/" element={<p>Home page of the /explore path</p>}/>
-         </Route>
-    </Routes>
+        <>
+            <NoAuthHeader />
+                <Routes>
+                    <Route element={<CheckUserIsNotAuth />}>
+                        <Route element={<LayOut />}>
+                            <Route path="/" element={<p>Home page of the /explore path</p>}/>
+                        </Route>
+                    </Route>
+                </Routes>
+            <NoAuthFooter />
+        </>
     )
 }
